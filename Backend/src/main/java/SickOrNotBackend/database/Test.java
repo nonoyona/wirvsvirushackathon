1d776b81b268feb6f71c329743a86e36efae9732
nonoyano <44346907+nonoyano@users.noreply.github.com>
2020-03-21 01:39:21 +0100

    test deleted

--- Backend/src/main/java/SickOrNotBackend/database/Test.java
+++ Backend/src/main/java/SickOrNotBackend/database/Test.java
@@ -2,15 +2,3 @@ package SickOrNotBackend.database;
 
 import java.net.UnknownHostException;
 
-public class Test {
-    public static void main(String[] args) {
-        try {
-            TestDataBase database = new TestDataBase();
-            database.insert("0", true);
-            System.out.println(database.search("0"));
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        
-    }
-}
--- Backend/src/main/java/SickOrNotBackend/database/TestDataBase.java
+++ Backend/src/main/java/SickOrNotBackend/database/TestDataBase.java
@@ -13,28 +13,3 @@ import org.bson.conversions.Bson;
 
 import SickOrNotBackend.datatypes.*;
 
-public class TestDataBase {
-    private MongoClient mongoClient;
-    private MongoCollection<Case> collection;
-    private MongoDatabase database;
-    private String databaseName = "test";
-
-    public TestDataBase() throws UnknownHostException {
-        mongoClient = MongoClients.create(
-                "mongodb://development:SWtxXHaxr7WW6eXb@db01.dev.schaefkn.com:27017/?authSource=admin&ssl=true");
-        database = mongoClient.getDatabase(databaseName);
-        collection = database.getCollection("testresults", Case.class);
-    }
-
-    public void insert(String id, boolean testResult) {
-        collection.insertOne(new Case(id, testResult));
-    }
-
-    public String search(String id) {
-        return collection.find(eq("id", id)).first().id;
-    }
-
-    private Bson eq(String string, String id) {
-        return ;
-    }
-}
