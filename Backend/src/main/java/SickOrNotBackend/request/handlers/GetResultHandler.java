d3d257a07f62a612b7ded636103feebe035b1564
fmundinger <finn@mundinger.de>
2020-03-21 15:26:59 +0100

    updated getresulthandler

--- Backend/src/main/java/SickOrNotBackend/request/handlers/GetResultHandler.java
+++ Backend/src/main/java/SickOrNotBackend/request/handlers/GetResultHandler.java
@@ -3,6 +3,7 @@ package SickOrNotBackend.request.handlers;
 
 import SickOrNotBackend.App;
 import SickOrNotBackend.datatypes.HealthType;
+import SickOrNotBackend.request.types.BadRequestResponse;
 import org.eclipse.jetty.http.HttpStatus;
 
 import io.javalin.http.Context;
@@ -16,9 +17,13 @@ public class GetResultHandler implements Handler {
     @Override
     public void handle(Context ctx) throws Exception {
         String id = ctx.pathParam("id");
-        HealthType health = App.database.getState(id);
-        ctx.status(HttpStatus.OK_200).json(health);
+        if (App.database.caseExists(id)) {
+            HealthType health = App.database.getState(id);
+            ctx.status(HttpStatus.OK_200).json(health);
+        }else{
+            ctx.status(HttpStatus.BAD_REQUEST_400).json(new BadRequestResponse("There is no case for the given id!"));
+        }
     }
 
-    
+
 }
